879cc5d2f6143f96f627300ba3ef85a8b0f1b4fd
sidiraos <raossidi@gmail.com>
2023-12-09 13:01:59 +0300

    add : features gif animated

--- src/components/PlayistMenu/Playist/Track.jsx
+++ src/components/PlayistMenu/Playist/Track.jsx
@@ -1,5 +1,6 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { setCurrentSong } from '../../../redux/Slices/musicStore';
+import SoundPlaying from '../../SoundPlaying';
 import React from 'react';
 const Track = ({ track }) => {
 	// console.log(track);
@@ -8,6 +9,7 @@ const Track = ({ track }) => {
 	const itemToFind = mySongs.find((song) => song.id === track.id);
 	const indexOfItem = mySongs.indexOf(itemToFind);
 	const currentSong = useSelector((state) => state.musics.currentSong);
+	const isSongLoading = useSelector((state) => state.musics.isSongLoading);
 	// console.log(currentSong);
 	// console.log(indexOfItem);
 	const handleClick = () => {
@@ -16,12 +18,15 @@ const Track = ({ track }) => {
 		// console.log(currentSong);
 	};
 	const bgCurrentSong = currentSong === indexOfItem ? 'bg-sky-400 text-black' : 'bg-sky-100 text-black';
+	const showAnimatePlaying = currentSong === indexOfItem && !isSongLoading ? <SoundPlaying /> : '';
+	
 	return (
 		<button
 			onClick={handleClick}
-			className={`rounded-md shadow-md px-3 py-4 cursor-pointer font-bold hover:bg-sky-400 hover:shadow-lg text-start ${bgCurrentSong}`}
-		>
-			{track.title} - {track.artist}
+			className={`rounded-md shadow-md px-3 py-4 cursor-pointer font-bold hover:bg-sky-400 hover:shadow-lg text-start ${bgCurrentSong} flex justify-between items-center`}
+		>	
+			<span>{track.title} - {track.artist}</span>
+			{showAnimatePlaying}
 		</button>
 	);
 };
---
+++ src/components/SoundPlaying.jsx
@@ -0,0 +1,12 @@
+const SoundPlaying = () => {
+	return (
+		<div>
+			<img
+				src="https://i.gifer.com/Z23N.gif"
+				alt="gif playing"
+				className="w-10 h-full"
+			/>
+		</div>
+	);
+};
+export default SoundPlaying;
