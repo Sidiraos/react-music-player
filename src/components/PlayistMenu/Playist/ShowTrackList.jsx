673dfe049b1958a038607cd9bc36156c2cc25790
sidiraos <raossidi@gmail.com>
2023-12-09 20:47:31 +0300

    fix: beug

--- src/components/PlayistMenu/Playist/ShowTrackList.jsx
+++ src/components/PlayistMenu/Playist/ShowTrackList.jsx
@@ -5,7 +5,7 @@ import React from 'react';
 const ShowTrackList = () => {
 	const mySongs = useSelector((state) => state.musics.mySongs);
 	return (
-		<div className=" max-w-xl mx-auto flex flex-col gap-4 mt-4 ">
+		<div className=" max-w-xl mx-auto flex flex-col gap-4 mt-4 select-none ">
 			{mySongs.map((song) => {
 				return <Track key={song.id} track={song} />;
 			})}
