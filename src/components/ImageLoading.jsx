1c0055306364f239b87d48593a3cea12528db125
sidiraos <raossidi@gmail.com>
2023-12-09 12:32:25 +0300

    modified : imageLoading

--- src/components/ImageLoading.jsx
+++ src/components/ImageLoading.jsx
@@ -1,12 +1,56 @@
-import loader from '../assets/loader.svg';
 const ImageLoading = () => {
 	return (
-		<div className="bg-transparent fixed left-5 top-36 shadow-xl hidden xl:block">
-			<img
-				src={loader}
-				alt="loader"
-				className="w-full h-64 object-cover"
-			/>
+		<div className="border border-transparent bg-transparent fixed left-5 top-36 shadow-xl hidden xl:block overflow-hidden">
+			<div className="animate-pulse w-64 h-64">
+				{/* <div className="rounded-full bg-slate-700 h-10 w-10"></div> */}
+				<div className="flex-1 space-y-6 py-1">
+					<div className="h-2 bg-slate-700 rounded"></div>
+					<div className="space-y-3">
+						<div className="grid grid-cols-3 gap-4">
+							<div className="h-2 bg-slate-700 rounded col-span-2"></div>
+							<div className="h-2 bg-slate-700 rounded col-span-1"></div>
+						</div>
+						<div className="grid grid-cols-3 gap-4">
+							<div className="h-2 bg-slate-700 rounded col-span-2"></div>
+							<div className="h-2 bg-slate-700 rounded col-span-1"></div>
+						</div>
+						<div className="grid grid-cols-3 gap-4">
+							<div className="h-2 bg-slate-700 rounded col-span-2"></div>
+							<div className="h-2 bg-slate-700 rounded col-span-1"></div>
+						</div>
+						<div className="grid grid-cols-3 gap-4">
+							<div className="h-2 bg-slate-700 rounded col-span-2"></div>
+							<div className="h-2 bg-slate-700 rounded col-span-1"></div>
+						</div>
+						<div className="grid grid-cols-3 gap-4">
+							<div className="h-2 bg-slate-700 rounded col-span-2"></div>
+							<div className="h-2 bg-slate-700 rounded col-span-1"></div>
+						</div>
+						<div className="grid grid-cols-3 gap-4">
+							<div className="h-2 bg-slate-700 rounded col-span-2"></div>
+							<div className="h-2 bg-slate-700 rounded col-span-1"></div>
+						</div>
+						<div className="grid grid-cols-3 gap-4">
+							<div className="h-2 bg-slate-700 rounded col-span-2"></div>
+							<div className="h-2 bg-slate-700 rounded col-span-1"></div>
+						</div>
+						<div className="grid grid-cols-3 gap-4">
+							<div className="h-2 bg-slate-700 rounded col-span-2"></div>
+							<div className="h-2 bg-slate-700 rounded col-span-1"></div>
+						</div>
+						<div className="grid grid-cols-3 gap-4">
+							<div className="h-2 bg-slate-700 rounded col-span-2"></div>
+							<div className="h-2 bg-slate-700 rounded col-span-1"></div>
+						</div>
+						<div className="grid grid-cols-3 gap-4">
+							<div className="h-2 bg-slate-700 rounded col-span-2"></div>
+							<div className="h-2 bg-slate-700 rounded col-span-1"></div>
+						</div>
+						<div className="h-2 bg-slate-700 rounded"></div>
+						<div className="h-2 bg-slate-700 rounded"></div>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
--- src/components/PlayistMenu/PlayistMenu.jsx
+++ src/components/PlayistMenu/PlayistMenu.jsx
@@ -5,6 +5,13 @@ const PlayistMenu = () => {
 	const mySongs = useSelector((state) => state.musics.mySongs);
 	const currentSong = useSelector((state) => state.musics.currentSong);
 	const cover = mySongs[currentSong]?.cover;
+	const showUICover = cover ? (
+		<div className="fixed left-5 top-36 shadow-xl hidden xl:block">
+			<img src={cover} alt="cover" className="w-full h-64 object-cover" />
+		</div>
+	) : (
+		<ImageLoading />
+	);
 
 	return (
 		<header className="bg-slate-800 h-[70vh] overflow-y-auto p-5 relative">
@@ -12,16 +19,7 @@ const PlayistMenu = () => {
 				PlayerMania - Your songs.
 			</h1>
 			<Playist />
-			{cover ? (
-				<div className="fixed left-5 top-36 shadow-xl hidden xl:block">
-					<img
-						src={cover}
-						alt="cover"
-						className="w-full h-64 object-cover"
-					/>
-				</div>
-			) : <ImageLoading />
-      }
+			{showUICover}
 		</header>
 	);
 };
